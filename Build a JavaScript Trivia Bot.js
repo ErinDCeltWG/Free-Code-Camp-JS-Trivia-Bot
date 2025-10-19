4cc0c5bdce419e43f56e65c14930d479791d9be0
ErinDCeltWG <107307280+ErinDCeltWG@users.noreply.github.com>
2025-10-19 10:05:29 -0500

    Add files via upload

---
+++ Build a JavaScript Trivia Bot.js
@@ -0,0 +1,29 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+let botLocation;
+
+botName = "Scot Bot";
+botLocation = "Galifrey";
+
+const botIntroduction = "My name is " + botName + " and I live on " + botLocation + ".";
+console.log(botIntroduction);
+
+let favoriteLanguage;
+
+favoriteLanguage = "Python";
+
+const botLanguage = "My favorite programming language is "
+  + favoriteLanguage + ".";
+console.log (botLanguage);
+
+let codingFact = "Creator Guido van Rossum chose the name " + favoriteLanguage + " as a tribute to the brilliant and surreal British comedy series Monty Python's Flying Circus.";
+console.log(codingFact); 
+
+ codingFact = "Zen and " + favoriteLanguage + "Tim Peters a Python contributor inseerted a poem about Python philosophies to read it type inport this in your Python IDLE";
+console.log(codingFact); 
+
+codingFact = "Like JavaScript " + favoriteLanguage + " is an interpreted language, meaning that it is executed line-by-line by an interpreter, as opposed to being compiled into machine code. This makes Python code easy to test and debug, enabling rapid prototyping and development. ";
+console.log(codingFact); 
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + "."); 
